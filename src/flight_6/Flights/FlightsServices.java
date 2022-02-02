4e1e059d6591f3571dbd28734acc77fc7af83055
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-02-02 12:31:33 +0000

    creating services

---
+++ src/flight_6/Flights/FlightsServices.java
@@ -0,0 +1,8 @@
+package flight_6.Flights;
+
+public class FlightsServices {
+
+
+    // 1. Check for null spaces in flights array - skip them if full
+    // x. Add user to flight
+}
---
+++ src/flight_6/User/UsersServices.java
@@ -0,0 +1,5 @@
+package flight_6.User;
+
+public class UsersServices {
+    //generate random unique id
+}
